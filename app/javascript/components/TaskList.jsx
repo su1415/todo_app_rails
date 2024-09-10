406be5c57f0355ee6f5e6eda8e44cbf1c8081ff9
su1415 <dummy@example.com>
2024-09-10 12:27:00 +0900

    feat: taskの表示

--- app/javascript/components/TaskList.jsx
+++ app/javascript/components/TaskList.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const TaskList = () => {
+function TaskList() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetch("/tasks")
+      .then((response) => response.json())
+      .then((data) => setTasks(data));
+  }, []);
+
   return (
-    <div>
-      <h1>Task List</h1>
-      <p>Here is where your tasks will appear.</p>
+    <div className="container mt-5">
+      <h1 className="text-center">Task List</h1>
+
+      <ul className="list-group">
+        { tasks.map((task) => (
+          <li key={ task.id } class="list-group-item d-flex justify-content-between align-items-center">
+            <div className="d-flex align-items-center flex-grow-1">
+              <span>{ task.title }</span>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-};
+}
 
 export default TaskList;
