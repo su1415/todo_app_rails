e4675eb8d7ffd3f51081b4d620c5cd7e4967c459
su1415 <dummy@example.com>
2024-09-10 14:55:03 +0900

    feat: taskの登録

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,18 @@ class TasksController < ApplicationController
     tasks = Task.all
     render json: tasks
   end
+
+  def create
+    task = Task.new(task_params)
+    unless task.save
+      return render json: task.errors, status: :unprocessable_entity
+    end
+    render json: task, status: :created
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title)
+  end
 end
--- app/javascript/components/TaskList.jsx
+++ app/javascript/components/TaskList.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 function TaskList() {
   const [tasks, setTasks] = useState([]);
+  const [addTitle, setAddTitle] = useState("");
 
   useEffect(() => {
     fetch("/tasks")
@@ -9,13 +10,47 @@ function TaskList() {
       .then((data) => setTasks(data));
   }, []);
 
+  function handleAddTask(e) {
+    e.preventDefault();
+    const csrfToken = document.querySelector("[name=csrf-token]").content;
+
+    fetch("/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRF-Token": csrfToken,
+      },
+      body: JSON.stringify({ task: { title: addTitle } }),
+    })
+      .then((response) => response.json())
+      .then((newTask) => {
+        setTasks([...tasks, newTask]);
+        setAddTitle("");
+      });
+  }
+
   return (
     <div className="container mt-5">
       <h1 className="text-center">Task List</h1>
 
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Add Task"
+          value={ addTitle }
+          onChange={ (e) => setAddTitle(e.target.value) }
+        />
+        <button
+          className="btn btn-primary"
+          onClick={ handleAddTask }>
+          Add
+        </button>
+      </div>
+
       <ul className="list-group">
         { tasks.map((task) => (
-          <li key={ task.id } class="list-group-item d-flex justify-content-between align-items-center">
+          <li key={ task.id } className="list-group-item d-flex justify-content-between align-items-center">
             <div className="d-flex align-items-center flex-grow-1">
               <span>{ task.title }</span>
             </div>
