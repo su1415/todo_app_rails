668209cbd7f10fbe8c330e8f8d4885e07ee44431
su1415 <dummy@example.com>
2024-09-10 10:40:44 +0900

    chore: Bootstrap のインストール

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import TaskList from "./components/TaskList.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 document.addEventListener("DOMContentLoaded", () => {
   const element = document.getElementById("task-list");
