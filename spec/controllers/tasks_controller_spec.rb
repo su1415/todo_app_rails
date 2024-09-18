441bbe64c2b28e0899378362139859d1a6045c7d
su1415 <dummy@example.com>
2024-09-18 12:40:43 +0900

    test: TasksController#updateのケースを追加

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,10 +1,10 @@
 require "rails_helper"
 
 RSpec.describe TasksController, type: :controller do
-  let!(:task_completed_test) { create(:task, title: "test completed task 1", completed: true) }
-  let!(:task_completed_sample) { create(:task, title: "sample completed task 2", completed: true) }
-  let!(:task_incompleted_test) { create(:task, title: "test incompleted task 1") }
-  let!(:task_incompleted_sample) { create(:task, title: "sample incompleted task 2") }
+  let!(:task_completed_test) { create(:task, title: "test completed task 1", due_date: Date.today + 1.days, completed: true) }
+  let!(:task_completed_sample) { create(:task, title: "sample completed task 2", due_date: Date.today + 2.days, completed: true) }
+  let!(:task_incompleted_test) { create(:task, title: "test incompleted task 1", due_date: Date.today + 3.days) }
+  let!(:task_incompleted_sample) { create(:task, title: "sample incompleted task 2", due_date: Date.today + 4.days) }
 
   describe "GET #index" do
     subject { get :index, format: :json, params: params }
@@ -117,7 +117,39 @@ RSpec.describe TasksController, type: :controller do
   end
 
   describe "PATCH/PUT #update" do
-    # TODO
+    let(:params) { { search: "test" } }
+    before { get :index, format: :json, params: params }
+    subject { patch :update, format: :json, params: { id: task_completed_test.id, task: { title: "test update task" } } }
+
+    context "更新が正常に完了" do
+      it "taskが更新されること" do
+        subject
+        expect(task_completed_test.reload.title).to eq "test update task"
+      end
+
+      it "HTTPステータスがokであること" do
+        subject
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "条件に一致したtasksが返ること" do
+        subject
+        json = JSON.parse(response.body)
+        expect(json.length).to eq(2)
+        expect(json.pluck("title")).to eq [task_completed_test.reload.title, task_incompleted_test.title]
+      end
+    end
+
+    context "更新がエラーで中断" do
+      before do
+        allow_any_instance_of(Task).to receive(:save).and_return(false)
+      end
+
+      it "HTTPステータスがunprocessable_entityであること" do
+        subject
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
   end
 
   describe "DELETE #destroy" do
